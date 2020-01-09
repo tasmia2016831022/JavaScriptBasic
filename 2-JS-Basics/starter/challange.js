a50785d6b332e086e435c87382b7cd552e14de88
Pathao Limited <pathaolimited@Pathaos-MacBook-Air.local>
2020-01-09 13:54:02 +0600

    Basic declaration and console

---
+++ 2-JS-Basics/starter/challange.js
@@ -0,0 +1,20 @@
+// BMI = mass/(height*height);
+// mass in kg and height in meter
+// store mass and height of Mark and Jhon
+// Calculate BMI
+// if Mark's is heigher than Jhon's
+// print in console
+
+var heightMark = 1
+var heightJhon = 1.2
+
+var massMark = 56;
+var massJhon = 67;
+
+var MarkBMI = massMark / ( heightMark * heightMark );
+var JhonBMI = massJhon / ( heightJhon * heightJhon );
+
+isMark = MarkBMI > JhonBMI;
+
+console.log(MarkBMI , JhonBMI);
+ console.log(isMark);
---
+++ 2-JS-Basics/starter/script.js
@@ -0,0 +1,73 @@
+//Data types
+/*var firstName = 'Jhon';
+console.log(firstName);
+var lastName ='Smith';
+var age = 28;
+var fullAge = true;
+console.log(fullAge);
+ var job;
+ console.log(job);
+ job = 'teacher'
+ console.log(job);
+*/
+ //variable mutation
+
+
+ /*
+ var firstName = 'jhon';
+ var age = 26;
+ console.log(firstName+' '+age);
+
+ var job,isMarried;
+ job = 'teacher';
+ isMarried = false;
+
+ console.log(firstName+' is a '+age+' year old '+
+ job+' Is he married? '+ isMarried);
+
+ age = 'twenty-six';
+ job = 'driver';
+
+ alert(firstName+' is a '+age+' year old '+
+ job+' Is he married? '+ isMarried);
+
+ var lastName = prompt('What is his last name ?')
+ console.log(firstName+' '+lastName);
+ */
+
+ //Basic Operations
+ 
+ /*
+ var thisYear, yearJhon, yearMark;
+ var ageJhon = 34;
+ var ageMark = 56;
+ thisYear = 2020;
+
+ yearJhon = thisYear - ageJhon;
+ yearMark = thisYear - ageMark;
+
+ console.log(yearJhon);
+ console.log(yearMark);
+
+
+ //Logical operator 
+ 
+ var jhonOlder = ageJhon > ageMark;
+ console.log(jhonOlder);
+
+ //typeof
+console.log(typeof jhonOlder);
+*/
+
+var now = 2018;
+var yearJhon = 1989;
+var fullAge = 18;
+
+var isFullAge = now - yearJhon >= fullAge;
+console.log(isFullAge);
+/*
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+*/
+ var x = (3+5)*4-6;
+ console.log(x);
+ 
