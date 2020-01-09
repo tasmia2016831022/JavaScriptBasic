e5e163f83f769bb1413b0acabffb00f7878c4461
Pathao Limited <pathaolimited@Pathaos-MacBook-Air.local>
2020-01-09 18:27:18 +0600

    section 2 done

--- 2-JS-Basics/starter/script.js
+++ 2-JS-Basics/starter/script.js
@@ -59,15 +59,110 @@ console.log(fullAge);
 console.log(typeof jhonOlder);
 */
 
-var now = 2018;
-var yearJhon = 1989;
-var fullAge = 18;
+// var now = 2018;
+// var yearJhon = 1989;
+// var fullAge = 18;
 
-var isFullAge = now - yearJhon >= fullAge;
-console.log(isFullAge);
-/*
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
-*/
- var x = (3+5)*4-6;
- console.log(x);
+// var isFullAge = now - yearJhon >= fullAge;
+// console.log(isFullAge);
+// /*
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+// */
+//  var x = (3+5)*4-6;
+//  console.log(x);
  
+
+//if-else
+
+// var firstName = 'Jhon';
+// var civilStatus = 'single';
+
+// if(civilStatus === 'married')
+// {
+//     console.log("Goood");
+// }
+// else{
+//     console.log("yep");
+// }
+
+//Ternary operator
+
+// var name = "kenon";
+// var age = 3;
+
+// age >= 17 ? console.log(name + ' can be a pilot')
+// : console.log(name + ' can be a boy');
+
+// var drink = age >= 18 ? 'beer' : 'juice';
+// console.log(drink);
+
+//switch
+
+// var job = 'driver';
+
+// switch(job){
+//     case 'teacher':
+//         console.log('teacher');
+//         break;
+//     case 'driver':
+//         console.log('car');
+//         break;
+//     default:
+//         console.log('nothing');
+// }
+
+//truthy and falsy values
+//falsy values : undefined , null , 0 , NaN
+//truthy values : Not a falsy values
+
+// var height = 0;
+//  if (height || height === 0){
+//      console.log('defined');
+//  }else{
+//      console.log('not yet defined');
+//  }
+
+
+
+//function
+
+// function calculateAge(birthYear){
+
+//     return 2020 - birthYear;
+// }
+
+// var age = calculateAge(1997);
+// console.log(age);
+
+//function expressions
+// var whatDoYouDo = function(job, firstname){
+//     switch(job){
+//         case 'as':
+//             return firstname + ' how to code';
+//         case 'mass':
+//             return firstname + ' yes';
+//         default:
+//             return firstname +   ' no';
+//     }
+// }
+
+// console.log(whatDoYouDo('mass','shema'));
+
+//Array
+// var names = ['Jhon','Mark'];
+// var years = new Array(12,13,14);
+
+//objects and properties
+
+// var jhon = {
+//     firstName: 'Jhon',
+//     birthYear: 1990,
+//     calcAge: function(){
+//         this.age = 2020 - this.birthYear;
+//     }
+// };
+
+// jhon.calcAge();
+// console.log(jhon);
+
+/
