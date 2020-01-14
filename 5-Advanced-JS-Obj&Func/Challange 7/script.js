af310ba3e8ff044524340a4c1195dc3d3c933f44
tasmia2016831022 <tasmia22@student.sust.edu>
2020-01-14 11:44:38 +0600

    Challange 7 done

--- 5-Advanced-JS-Obj&Func/Challange 7/script.js
+++ 5-Advanced-JS-Obj&Func/Challange 7/script.js
@@ -13,12 +13,21 @@
         }
     }
     
-    Question.prototype.checkAns = function(ans){
+    Question.prototype.checkAns = function(ans,callback){
+        var sc;
         if(ans === this.correct){
-            console.log('Correct Answer !!')
+            console.log('Correct Answer !!');
+            sc = callback(true);
         }else{
-            console.log('Try Again!')
+            console.log('Try Again!');
+            sc = callback(false);
         }
+        this.displayScore(sc);
+     }
+
+     Question.prototype.displayScore = function(score){
+         console.log('Score is:'+score);
+         console.log('----------------');
      }
     
     var q1 = new Question('Which drink has more caffaine?', ['juice','coffee'],1 );
@@ -26,29 +35,38 @@
     var q3 = new Question('Which color has shorter wave-length', ['red','blue','green'],1);
     
     var questions = [q1,q2,q3];
+
+    function score(){
+        var sc = 0;
+        return function(correct){
+            if(correct){
+                sc++
+            }
+            return  sc;
+        }
+    }
+
+    var keepScore = score();
     
-     function nextQuestion(){
+    function nextQuestion(){
     
         var n = Math.floor(Math.random() * questions.length);
         
-        //onsole.log(n);
+        //console.log(n);
         
         questions[n].displayQuestion();
         
         var q = questions[n].question +'\n'+ questions[n].answers.join('\n');
         
-        var answer = parseInt(prompt(q));
-        
-        
+        var answer = prompt(q);
 
         if(answer !== 'exit'){
-            questions[n].checkAns(answer);
+            questions[n].checkAns(parseInt(answer),keepScore);
             nextQuestion();
         }
         
      }
-
+     
      nextQuestion();
-    
     
 })();
