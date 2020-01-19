39f58dbe8aa21f09276eaccb6930fd0df22e58b7
tasmia2016831022 <tasmia22@student.sust.edu>
2020-01-19 18:07:39 +0600

    section 8 done :3

--- 8-AsyncJS-Promises-AsyncAwait-Ajax/ajaxCall.js
+++ 8-AsyncJS-Promises-AsyncAwait-Ajax/ajaxCall.js
@@ -3,7 +3,7 @@ function getWeather(woeid) {
     `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`
   )
     .then(result => {
-      console.log(result);
+      //console.log(result);
       return result.json();
     })
     .then(data => {
@@ -18,3 +18,27 @@ function getWeather(woeid) {
 
 getWeather(2487956);
 getWeather(44418);
+
+async function getWeatherAW(woeid) {
+  try {
+    const result = await fetch(
+      `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`
+    );
+    const data = await result.json();
+    const tomorrow = data.consolidated_weather[1];
+    console.log(
+      `Tenparature in ${data.title} stay between ${tomorrow.min_temp} and ${tomorrow.max_temp} tomorrow `
+    );
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getWeatherAW(2487956);
+
+let dataLondon;
+getWeatherAW(44418).then(data => {
+    dataLondon = data;
+    console.log(dataLondon);
+});
