c19c85b05370d3279d7e7b71525e6f722a358f24
tasmia2016831022 <tasmia22@student.sust.edu>
2020-01-20 19:08:23 +0600

    pagination done

--- 9-ModernJS-ES6-NPM-Babel-Webpack/Forkify/src/js/index.js
+++ 9-ModernJS-ES6-NPM-Babel-Webpack/Forkify/src/js/index.js
@@ -40,3 +40,14 @@ import {elements, renderLoader, clearLoader} from './views/base';
      controlSearch();
  });
 
+ elements.searchResPages.addEventListener('click', e=>{
+     const btn = e.target.closest('.btn-inline');
+     if(btn){
+         const goToPage = parseInt(btn.dataset.goto,10);
+         searchView.clearResults();
+         searchView.renderResults(state.search.results, goToPage);
+         console.log(goToPage);
+     }
+     
+ });
+
