44d595748902dfd796e5428eec53fea9ab93ebfd
tasmia2016831022 <tasmia22@student.sust.edu>
2020-01-20 12:48:49 +0600

    babel set up done. install @babel/core additional

--- 9-ModernJS-ES6-NPM-Babel-Webpack/Forkify/src/js/index.js
+++ 9-ModernJS-ES6-NPM-Babel-Webpack/Forkify/src/js/index.js
@@ -1,4 +1,5 @@
 // Global app controller
 import num from './test';
+const x = 23;
 
-console.log(`I imported ${num} from another module called test. `);
+console.log(`I imported ${num} from another module called test. Variable x is ${x}. `);
--- 9-ModernJS-ES6-NPM-Babel-Webpack/Forkify/webpack.config.js
+++ 9-ModernJS-ES6-NPM-Babel-Webpack/Forkify/webpack.config.js
@@ -1,19 +1,30 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/js/index.js",
+  entry:['babel-polyfill', './src/js/index.js'],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "js/bundle.js"
   },
   devServer: {
-    contentBase: './dist'
+    contentBase: "./dist"
   },
   plugins: [
     new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: './src/index.html'
+      filename: "index.html",
+      template: "./src/index.html"
     })
-]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
 };
