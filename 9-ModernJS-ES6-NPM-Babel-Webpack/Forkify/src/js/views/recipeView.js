8cf8f40c443c3266daeb0f3b9c105c3fe365f2e1
tasmia2016831022 <tasmia22@student.sust.edu>
2020-01-22 17:15:37 +0600

    Forkify project completed

--- 9-ModernJS-ES6-NPM-Babel-Webpack/Forkify/src/js/views/recipeView.js
+++ 9-ModernJS-ES6-NPM-Babel-Webpack/Forkify/src/js/views/recipeView.js
@@ -1,29 +1,32 @@
-import {elements} from './base';
-import {Fraction} from 'fractional';
+import { elements } from "./base";
+import { Fraction } from "fractional";
 
-export const clearRecipe =() =>{
-    elements.recipe.innerHTML = '';
-}
+export const clearRecipe = () => {
+  elements.recipe.innerHTML = "";
+};
 
 const formatCount = count => {
-    if(count){
-        const newCount = Math.round(count * 10000) / 10000;
-        const [int, dec] = newCount.toString().split('.').map(el => parseInt(el, 10));
+  if (count) {
+    const newCount = Math.round(count * 10000) / 10000;
+    const [int, dec] = newCount
+      .toString()
+      .split(".")
+      .map(el => parseInt(el, 10));
 
-        if (!dec) return newCount;
+    if (!dec) return newCount;
 
-        if (int === 0) {
-            const fr = new Fraction(newCount);
-            return `${fr.numerator}/${fr.denominator}`;
-        } else {
-            const fr = new Fraction(newCount - int);
-            return `${int} ${fr.numerator}/${fr.denominator}`;
-        }
+    if (int === 0) {
+      const fr = new Fraction(newCount);
+      return `${fr.numerator}/${fr.denominator}`;
+    } else {
+      const fr = new Fraction(newCount - int);
+      return `${int} ${fr.numerator}/${fr.denominator}`;
     }
-    return '?';
-}
+  }
+  return "?";
+};
 
-const createIngredient = ingredient =>`
+const createIngredient = ingredient => `
     <li class="recipe__item">
         <svg class="recipe__icon">
             <use href="img/icons.svg#icon-check"></use>
@@ -37,10 +40,12 @@ const createIngredient = ingredient =>`
 
 `;
 
-export const renderRecipe = (recipe,isLiked) => {
+export const renderRecipe = (recipe, isLiked) => {
   const markup = `
             <figure class="recipe__fig">
-                <img src="${recipe.img}" alt="${recipe.title}" class="recipe__img">
+                <img src="${recipe.img}" alt="${
+    recipe.title
+  }" class="recipe__img">
                     <h1 class="recipe__title">
                         <span>${recipe.title}</span>
                     </h1>
@@ -50,14 +55,18 @@ export const renderRecipe = (recipe,isLiked) => {
                     <svg class="recipe__info-icon">
                         <use href="img/icons.svg#icon-stopwatch"></use>
                     </svg>
-                    <span class="recipe__info-data recipe__info-data--minutes">${recipe.time}</span>
+                    <span class="recipe__info-data recipe__info-data--minutes">${
+                      recipe.time
+                    }</span>
                     <span class="recipe__info-text"> minutes</span>
                 </div>
                 <div class="recipe__info">
                     <svg class="recipe__info-icon">
                         <use href="img/icons.svg#icon-man"></use>
                     </svg>
-                        <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
+                        <span class="recipe__info-data recipe__info-data--people">${
+                          recipe.servings
+                        }</span>
                         <span class="recipe__info-text"> servings</span>
 
                     <div class="recipe__info-buttons">
@@ -75,13 +84,17 @@ export const renderRecipe = (recipe,isLiked) => {
                 </div>
                 <button class="recipe__love">
                     <svg class="header__likes">
-                    <use href="img/icons.svg#icon-heart${isLiked ? '' : '-outlined'}"></use>
+                    <use href="img/icons.svg#icon-heart${
+                      isLiked ? "" : "-outlined"
+                    }"></use>
                     </svg>
                 </button>
                 </div>
             <div class="recipe__ingredients">
                 <ul class="recipe__ingredient-list">
-                    ${recipe.ingredients.map(el => createIngredient(el)).join("")}
+                    ${recipe.ingredients
+                      .map(el => createIngredient(el))
+                      .join("")}
                 </ul> 
             <button class="btn-small recipe__btn recipe__btn--add">
                 <svg class="search__icon">
@@ -95,9 +108,13 @@ export const renderRecipe = (recipe,isLiked) => {
             <h2 class="heading-2">How to cook it</h2>
             <p class="recipe__directions-text">
                     This recipe was carefully designed and tested by
-                    <span class="recipe__by">${recipe.author}</span>. Please check out directions at their website.
+                    <span class="recipe__by">${
+                      recipe.author
+                    }</span>. Please check out directions at their website.
             </p>
-            <a class="btn-small recipe__btn" href="${recipe.url}" target="_blank">
+            <a class="btn-small recipe__btn" href="${
+              recipe.url
+            }" target="_blank">
                 <span>Directions</span>
                 <svg class="search__icon">
                     <use href="img/icons.svg#icon-triangle-right"></use>
@@ -106,16 +123,16 @@ export const renderRecipe = (recipe,isLiked) => {
     </div>
     `;
 
-    elements.recipe
-    .insertAdjacentHTML('afterbegin',markup);
+  elements.recipe.insertAdjacentHTML("afterbegin", markup);
 };
 export const updateServingsIngredients = recipe => {
-    // Update servings
-    document.querySelector('.recipe__info-data--people').textContent = recipe.servings;
+  // Update servings
+  document.querySelector(".recipe__info-data--people").textContent =
+    recipe.servings;
 
-    // Update ingredeints
-    const countElements = Array.from(document.querySelectorAll('.recipe__count'));
-    countElements.forEach((el, i) => {
-        el.textContent = formatCount(recipe.ingredients[i].count);
-    });
+  // Update ingredeints
+  const countElements = Array.from(document.querySelectorAll(".recipe__count"));
+  countElements.forEach((el, i) => {
+    el.textContent = formatCount(recipe.ingredients[i].count);
+  });
 };
