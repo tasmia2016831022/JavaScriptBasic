cf7f28788ffa88d2c5c8e2bdaeebee5309580e54
tasmia2016831022 <tasmia22@student.sust.edu>
2020-01-23 11:31:07 +0600

    not done yet, will do later

--- 10-NodeJS-LaptopProject/Laptop Project/index.js
+++ 10-NodeJS-LaptopProject/Laptop Project/index.js
@@ -1,3 +1,28 @@
-console.log('Hello');
-console.log('key');
-console.log('lie');
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const json = fs.readFileSync(`${__dirname}/data/data.json`,'utf-8');
+const laptopdata = JSON.parse(json);
+
+const server = http.createServer((req,res) =>{
+
+    const pathName = url.parse(req.url,true).pathname;
+    const query = url.parse(req.url,true).query;
+
+    if(pathName === '/products' || pathName === '/'){
+        res.writeHead(200,{'Content-type': 'text/html'});
+        res.end('This is the product page!');
+    }
+    else if(pathName === '/laptop'){
+        res.writeHead(200,{'Content-type': 'text/html'});
+        res.end('This is the laptop page!');
+    }else{
+        res.writeHead(404,{'Content-type': 'text/html'});
+        res.end('Page not found!!');
+    }
+    //console.log('Someone has accessed the server!!');
+});
+server.listen(1337, '127.0.0.1', ()=>{
+    console.log('Server listening');
+});
