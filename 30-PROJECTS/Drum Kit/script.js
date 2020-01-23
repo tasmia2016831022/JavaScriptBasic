35d4853ee37d1e9a58324b38dfa716ded90291f9
tasmia2016831022 <tasmia22@student.sust.edu>
2020-01-23 16:40:55 +0600

    drum kit working

--- 30-PROJECTS/Drum Kit/script.js
+++ 30-PROJECTS/Drum Kit/script.js
@@ -1,4 +1,3 @@
-
 window.addEventListener('keydown', function(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
@@ -8,3 +7,15 @@ window.addEventListener('keydown', function(e){
     audio.play();
     key.classList.add('playing');
 });
+
+function removeTransition(e){
+    if(e.propertyName !== 'transform')return;
+    console.log(this);
+    this.classList.remove('playing');
+    console.log('====', this.classList);
+
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+console.log(keys);
