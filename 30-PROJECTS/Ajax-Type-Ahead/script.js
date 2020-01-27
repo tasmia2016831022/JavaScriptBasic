b6f01a92deb0367b7ebad1c96c5dcd0f2dac5842
tasmia2016831022 <tasmia22@student.sust.edu>
2020-01-27 11:55:39 +0600

    Ajax call done

--- 30-PROJECTS/Ajax-Type-Ahead/script.js
+++ 30-PROJECTS/Ajax-Type-Ahead/script.js
@@ -4,8 +4,39 @@ const cities = [];
 fetch(endpoint)
     .then(blob => blob.json())
     .then(data => cities.push(...data));
+
 function findMatches(wordToMatch, cities){
     return cities.filter(place =>{
         const regex = new RegExp(wordToMatch, 'gi');
+        return place.city.match(regex) || place.state.match(regex);
     });
 }
+
+function numWithCommas(num){
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+}
+
+function displayMatches(){
+    const matchArray = findMatches(this.value, cities);
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value,'gi');
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+        return `
+        <li>
+            <span class="name">${cityName}, ${stateName}</span>
+            <span class="population">${numWithCommas(place.population)}</span>
+        </li>
+        `;
+
+    }).join('');
+    suggestions.innerHTML = html;
+    
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
