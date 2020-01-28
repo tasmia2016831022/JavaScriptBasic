822b2a2b8376a25578a5744cef1aaa816e3877c3
tasmia2016831022 <tasmia22@student.sust.edu>
2020-01-28 12:20:50 +0600

    image animation done

--- 30-PROJECTS/Side-In-on-scroll/script.js
+++ 30-PROJECTS/Side-In-on-scroll/script.js
@@ -1,28 +1,36 @@
 function debounce(func, wait = 20, immediate = true) {
-    var timeout;
-    return function() {
-      var context = this, args = arguments;
-      var later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      var callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+  var timeout;
+  return function() {
+    var context = this,
+      args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
     };
-  }
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
 
-  const sliderImages = document.querySelectorAll('.slide-in');
+const sliderImages = document.querySelectorAll(".slide-in");
 
+function checkSlide(e) {
+  sliderImages.forEach(sliderImage => {
+    //Halfway through the image
+    const slideInAt =
+      window.scrollY + window.innerHeight - sliderImage.height / 2;
+    //bottom of the image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add("active");
+    } else {
+      sliderImage.classList.remove("active");
+    }
+  });
+}
 
-
-  function checkSlide(e){
-      sliderImages.forEach(sliderImage => {
-          const slideInAt = (window.scrollY + window.innerHeight);
-          console.log(slideInAt);
-
-      });
-  }
-
-  window.addEventListener('scroll',debounce(checkSlide));
+window.addEventListener("scroll", debounce(checkSlide));
