f25cd70120339713590674dbfe4223735366b47b
tasmia2016831022 <tasmia22@student.sust.edu>
2020-02-02 11:34:12 +0600

    whack a mole game done

--- 30-PROJECTS/Whack-A-Mole-Game/script.js
+++ 30-PROJECTS/Whack-A-Mole-Game/script.js
@@ -2,6 +2,8 @@ const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 let lastHole;
+let timeUp = false;
+let score = 0;
 
 function randTime(min, max){
     return Math.round(Math.random()* (max-min) + min);
@@ -11,10 +13,37 @@ function randHole(holes){
     const indx = Math.floor(Math.random()*holes.length);
     const hole = holes[indx];
     if(hole === lastHole){
-        return randomeHole(holes);
+        return randHole(holes);
     }
-    console.log(hole);
 
     lastHole = hole;
     return hole;
 }
+
+function peep(){
+    const time = randTime(200,1000);
+    const hole = randHole(holes);
+    hole.classList.add('up');
+    setTimeout(()=>{
+        hole.classList.remove('up');
+        if(!timeUp)peep();
+    },time)
+
+}
+
+function startGame(){
+    scoreBoard.textContent = 0;
+    timeUp = false ;
+    score = 0;
+    peep();
+    setTimeout(() => timeUp = true,60000);
+}
+
+function bonk(e){
+    if(!e.isTrusted)return; // fake
+    score++;
+    this.classList.remove('up');
+    scoreBoard.textContent = score;
+}
+
+moles.forEach(mole => mole.addEventListener('click',bonk));
